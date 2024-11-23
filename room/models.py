189409e560ad3acc9c45c392c00d8733d1b33421
Elvis Kolbire Bire <bireelvis@gmail.com>
2024-11-23 23:38:40 +0000

    changes to the user models.py and template/profile.html

--- room/models.py
+++ room/models.py
@@ -7,11 +7,12 @@ class User(AbstractUser):
     name = models.CharField (max_length=200, null =True)
     email = models.EmailField(unique=True)
     bio = models.TextField( null=True)
+    # username = models.CharField(null= True, max_length=200)
 
     avatar = models.ImageField(null=True, default= "avatar.svg")
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['username']
 
 # Create your models here.
 class Topic(models.Model):
